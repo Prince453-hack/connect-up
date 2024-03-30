f36e27de9edf514e13af6a6c577efc1e13388d2a
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-03-30 23:58:36 +0530

    dialog ui

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -11,13 +11,15 @@ const MeetingTypeList = () => {
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
 
+  const createMeeting = () => {};
+
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
         img="/icons/add-meeting.svg"
         title="New Meeting"
         description="Start an instant meeting"
-        handleClick={() => setMeetingState("isJoiningMeeting")}
+        handleClick={() => setMeetingState("isInstantMeeting")}
         className="bg-orange-1"
       />
       <HomeCard
@@ -42,7 +44,14 @@ const MeetingTypeList = () => {
         className="bg-yellow-1"
       />
 
-      <MeetingModal />
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+      />
     </section>
   );
 };
